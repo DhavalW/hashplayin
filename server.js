0ae11a2abdfb2258d1d2de95dab3a53c2e109daa
DhavalW <DhavalW@Dhavals-MacBook-Pro.local>
2017-01-31 17:49:30 +0530

    added server.js
    + stub files

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var http = require('http')
+	.Server(app);
+
+
+
+// fs.readFile('package.json', function (err, data) {
+// 	console.log('FILEDATA is ', data.toString());
+// 	return data;
+// });
+
+
+// app.get('/', function (req, res) {
+// 	res.send('<h1>Hello world</h1>');
+// });
+app.get('/', express.static(__dirname +'/www/' /*, { maxAge: oneDay }*/ ));
+app.get('/DIYDM', express.static(__dirname +'/www/diydm/' /*, { maxAge: oneDay }*/ ));
+
+var oneDay = 86400000;
+
+//app.use(express.compress());
+// console.log('Routes are :');
+// console.log('/sfui/composer\t\t=\t'+ __dirname +'/clients/sfui-composer');
+// console.log('/sfui/demos/calculator\t=\t'+__dirname + '/clients/demos/calculator');
+
+// app.use('/demos/calculator', express.static(__dirname + '/www/demos/calculator' /*, { maxAge: oneDay }*/ ));
+// app.use('/static', express.static(__dirname + '/www/static' /*, { maxAge: oneDay }*/ ));
+// app.use('/node_modules', express.static(__dirname + '/www/node_modules' /*, { maxAge: oneDay }*/ ));
+
+
+
+http.listen(process.env.PORT || 5000, function () {
+	console.log('listening on port ',process.env.PORT || '5000');
+});
