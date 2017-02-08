cc4cf0ea31d1037a0e3615eca56330405fe4ca27
DhavalW <DhavalW@Dhavals-MacBook-Pro.local>
2017-02-08 16:40:27 +0530

    no message

--- www/static/js/main-controller.js
+++ www/static/js/main-controller.js
@@ -4,11 +4,62 @@ angular.module("hashplayin")
 
 function mainController ($scope){
 
+	// $scope.visitor = {
+	// 	artisteName : '',
+	// 	firstName :'',
+	// 	emailId:''
+	// };
 	$scope.visitor = {
-		artisteName : "",
-		firstName :'',
-		emailId:''
+		artisteName : 'Indian Ocean',
+		firstName :'Dhaval',
+		emailId:'dwathare@gmail.com',
+		fanCount:100000
+
 	};
+
+
+	$scope.frames = {
+		number:1
+	};
+
+	$scope.story = {
+		track:'createABuzz',
+		version:1
+	};
+
+	$scope.setStory= function(trk,ver){
+		$scope.story = {
+			track:trk,
+			version:ver
+		};
+		$scope.frames.number = 3;
+	};
+
+	var nextStory = function(){
+		switch($scope.story.track){
+			case 'createABuzz':
+				$scope.setStory('planATour',1);
+				break;
+			case 'planATour':
+				$scope.setStory('tapSuperfans',1);
+				break;
+			case 'tapSuperfans':
+				$scope.setStory('raiseFunds',1);
+				break;
+			case 'raiseFunds':
+				$scope.setStory('growFanbase',1);
+				break;
+		}
+
+	};
+
+	var nextFrame = function(){
+		$scope.frames.number++;
+	};
+	var prevFrame = function(){
+		$scope.frames.number--;
+	};
+
 
 	$scope.display = {
 		showEmailInput : false
@@ -28,7 +79,6 @@ function mainController ($scope){
 
 		if (keyEvent.which === 13){
 			$scope.display.showEmailInput = siteState.artisteNameEntered;
-
 		}
 	};
 
@@ -36,4 +86,49 @@ function mainController ($scope){
 		$scope.display.showEmailInput = siteState.artisteNameEntered;
 	};
 
+	$scope.onGoClick = function(){
+		if ( ($scope.visitor.firstName.length >0 && $scope.visitor.firstName.trim() !="")
+			&& ( $scope.visitor.emailId.length >0 && $scope.visitor.emailId.trim() !="") ) {
+
+			nextFrame();
+		}
+	};
+
+	$scope.fanCountKeyPress = function(keyEvent){
+		if(keyEvent.which == 13 && $scope.story.version == 0){
+			$scope.setStory("createABuzz",1);
+		}
+	};
+
+	$scope.fanCountBlur = function(){
+		$scope.setStory("createABuzz",1);
+	};
+
+	$scope.cycleStage2Frames = function(keyEvent){
+		if (keyEvent.which === 32){
+			if($scope.frames.number >= 3 && $scope.story.version >0){
+				nextFrame();
+				if ($scope.frames.number >7)
+					nextStory();
+			}
+
+			if ($scope.story.version == 0){
+				$scope.setStory('createABuzz',1);
+			}
+		}
+		if(keyEvent.which === 13){
+			if($scope.frames.number > 3 && $scope.story.version >0){
+				prevFrame();
+			}
+		}
+
+	};
+
+	$scope.onFrameClick = function(){
+		if($scope.frames.number >= 3 && $scope.story.version >0){
+			nextFrame();
+			if ($scope.frames.number >7)
+				nextStory();
+		}
+	}
 };
